b778137af44486ede8899d94ef3f5f087a0745ce
AnByeongDo <abd07180@naver.com>
2024-01-14 20:10:38 +0900

    test2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.proj"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.proj"
-        minSdk = 24
+        minSdk = 16
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "proj"
 include(":app")
+ 
