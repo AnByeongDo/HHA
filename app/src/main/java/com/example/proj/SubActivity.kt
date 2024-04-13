dc31d8d1a1460c4d4fc07de01567ae091e28fc2f
koogw <koogw3888@gmail.com>
2024-04-14 00:02:48 +0900

    page

--- app/src/main/java/com/example/proj/SubActivity.kt
+++ app/src/main/java/com/example/proj/SubActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -27,21 +28,29 @@ class SubActivity : AppCompatActivity() {
                 // 선택된 이미지가 있을 경우 ImageView에 설정
                 imageView?.setImageURI(uri)
             }
+
         }
 
         button?.setOnClickListener {
             // 갤러리로 이동하는 Intent를 실행
             getContent.launch("image/*")
         }
+
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
             1 -> if (resultCode == RESULT_OK) {
                 val uri = data!!.data
                 imageView!!.setImageURI(uri)
             }
+
         }
+        finish()
+
     }
+
 }
